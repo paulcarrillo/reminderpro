8d3ab1850bb0b130a2c3b0211b9584a692e09048
Paul Carrillo <paulcarrillo05@gmail.com>
2017-09-08 19:00:25 -0600

    Clear all button action was created and the app was complete

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-  import { ADD_REMINDER, DELETE_REMINDER} from '../constants';
+  import { ADD_REMINDER, DELETE_REMINDER, CLEAR_REMINDERS } from '../constants';
   import { bake_cookie, read_cookie } from 'sfcookies';
 
   const reminder = (action) => {
@@ -28,7 +28,11 @@
         reminders = removeById(state, action.id);
         bake_cookie('reminders', reminders);
         return reminders;
-      default:
+      case CLEAR_REMINDERS:
+        reminders = [];
+        bake_cookie('reminders', reminders);
+        return reminders;
+        default:
         return state;
     }
   }
